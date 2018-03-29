6f3d795d31f6f190775feeab8b0cc78d27fc60ad
Adam Sitnik <adam.sitnik@gmail.com>
2018-03-29 22:36:22 +0200

    introduce builder for CustomCoreClrToolchain to make it easier to configure it, #700 #702

--- tests/BenchmarkDotNet.IntegrationTests/CustomCoreClrToolchainTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/CustomCoreClrToolchainTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if NETCOREAPP2_1
+using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text.RegularExpressions;
@@ -20,21 +21,111 @@ namespace BenchmarkDotNet.IntegrationTests
         {
             var config = ManualConfig.CreateEmpty()
                 .With(Job.Dry.With(
-                    CustomCoreClrToolchain.CreateForNightlyCoreFxBuild("4.5.0-preview3-26328-01")));
+                    CustomCoreClrToolchain.CreateBuilder()
+                        .UseCoreClrDefault()
+                        .UseCoreFxNuGet("4.5.0-preview3-26328-01")
+                        .ToToolchain()));
 
             CanExecute<CheckCoreFxVersion>(config);
         }
-    }
 
-    public class CheckCoreFxVersion
-    {
-        [Benchmark]
-        public void Check()
+        public class CheckCoreFxVersion
         {
-            var corefxAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(Regex).GetTypeInfo().Assembly.Location);
+            [Benchmark]
+            public void Check()
+            {
+                var coreFxAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(Regex).GetTypeInfo().Assembly.Location);
 
-            if (corefxAssemblyInfo.FileVersion != "4.6.26328.01")
-                throw new InvalidOperationException("Wrong version");
+                if (coreFxAssemblyInfo.FileVersion != "4.6.26328.01")
+                    throw new InvalidOperationException($"Wrong CoreFx version: was {coreFxAssemblyInfo.FileVersion}, should be 4.6.26328.01");
+            }
+        }
+
+        [Fact]
+        public void CanBenchmarkGivenCoreClrMyGetBuild()
+        {
+            var config = ManualConfig.CreateEmpty()
+                .With(Job.Dry.With(
+                    CustomCoreClrToolchain.CreateBuilder()
+                        .UseCoreFxDefault()
+                        .UseCoreClrNuGet("2.1.0-preview3-26329-08")
+                        .ToToolchain()));
+
+            CanExecute<CheckCoreClrVersion>(config);
+        }
+
+        public class CheckCoreClrVersion
+        {
+            [Benchmark]
+            public void Check()
+            {
+                var coreClrAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(object).GetTypeInfo().Assembly.Location);
+
+                if (coreClrAssemblyInfo.FileVersion != "4.6.26329.08")
+                    throw new InvalidOperationException($"Wrong CoreClr version: was {coreClrAssemblyInfo.FileVersion}, should be 4.6.26329.08");
+            }
+        }
+
+        [Fact]
+        public void CanBenchmarkGivenCoreClrAndCoreFxMyGetBuilds()
+        {
+            var config = ManualConfig.CreateEmpty()
+                .With(Job.Dry.With(
+                    CustomCoreClrToolchain.CreateBuilder()
+                        .UseCoreFxNuGet("4.5.0-preview3-26328-01")
+                        .UseCoreClrNuGet("2.1.0-preview3-26329-08")
+                        .ToToolchain()));
+
+            CanExecute<CheckCoreClrAndCoreFxVersions>(config);
+        }
+
+        public class CheckCoreClrAndCoreFxVersions
+        {
+            [Benchmark]
+            public void Check()
+            {
+                var coreFxAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(Regex).GetTypeInfo().Assembly.Location);
+
+                if (coreFxAssemblyInfo.FileVersion != "4.6.26328.01")
+                    throw new InvalidOperationException($"Wrong CoreFx version: was {coreFxAssemblyInfo.FileVersion}, should be 4.6.26328.01");
+
+                var coreClrAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(object).GetTypeInfo().Assembly.Location);
+
+                if (coreClrAssemblyInfo.FileVersion != "4.6.26329.08")
+                    throw new InvalidOperationException($"Wrong CoreFx version: was {coreClrAssemblyInfo.FileVersion}, should be 4.6.26329.08");
+            }
+        }
+
+        [Fact(Skip = "You need to setup CoreFx and CoreClr on your machine to run it and update the paths..")]
+        public void CanBenchmarkGivenLocalCoreClrAndCoreFxBuilds()
+        {
+            var config = ManualConfig.CreateEmpty()
+                .With(Job.Dry.With(
+                    CustomCoreClrToolchain.CreateBuilder()
+                        .UseCoreFxLocalBuild("4.5.0-preview3-26427-0", @"C:\Projects\corefx\bin\packages\Debug")
+                        .UseCoreClrLocalBuild("2.1.0-preview3-26420-0", @"C:\Projects\coreclr\bin\Product\Windows_NT.x64.Debug\.nuget\pkg", @"C:\Projects\coreclr\packages\")
+                        .UseNuGetClearTag(false) // for now I need it
+                        .ToToolchain()));
+
+            CanExecute<CheckLocalCoreClrAndCoreFxVersions>(config);
+        }
+
+        public class CheckLocalCoreClrAndCoreFxVersions
+        {
+            [Benchmark]
+            public void Check()
+            {
+                var coreFxAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(Regex).GetTypeInfo().Assembly.Location);
+
+                if (coreFxAssemblyInfo.FileVersion != "4.6.26427.0")
+                    throw new InvalidOperationException($"Wrong CoreFx version: was {coreFxAssemblyInfo.FileVersion}, should be 4.6.26427.0");
+
+                var coreClrAssemblyInfo = FileVersionInfo.GetVersionInfo(typeof(object).GetTypeInfo().Assembly.Location);
+
+                if (coreClrAssemblyInfo.FileVersion != "4.6.26420.0")
+                    throw new InvalidOperationException($"Wrong CoreFx version: was {coreClrAssemblyInfo.FileVersion}, should be 4.6.26420.0");
+            }
         }
     }
 }
+#endif
